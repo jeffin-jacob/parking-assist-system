d1b3e048221cd2048e71475d8c8d93d764ee085f
jeffinjacob <jeffinjac@gmail.com>
2025-11-11 03:14:41 -0500

    ran make clean and recommitted

--- phase-2/build/core/hooks.c.d
+++
@@ -1,2 +0,0 @@
-/Users/jeffinjacob/Documents/cse321/parking-assist-system/phase-2/build/core/hooks.c.o: \
- /Users/jeffinjacob/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/hooks.c
