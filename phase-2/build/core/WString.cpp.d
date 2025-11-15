0b52d553fd7e925aa913e5a1d60267304cdea112
jeffinjacob <jeffinjac@gmail.com>
2025-11-15 04:11:56 -0500

    sensor finds distance

--- phase-2/build/core/WString.cpp.d
+++
@@ -1,3 +0,0 @@
-/Users/jeffinjacob/Documents/cse321/parking-assist-system/phase-2/build/core/WString.cpp.o: \
- /Users/jeffinjacob/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/WString.cpp \
- /Users/jeffinjacob/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/WString.h
